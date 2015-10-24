bf4bb35e9a119fcf972a7906a663b50a29a65109
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2015-10-24 15:54:24 +0100

    Adapt to do exercise 2

--- lab-1.pl
+++ lab-1.pl
@@ -1,12 +1,17 @@
 numeral(0).
 numeral(s(X)) :- numeral(X).
+numeral(p(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
+add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
-eval(s(X+Y),s(Z)) :- eval(X,A), eval(Y,B), add(A,B,Z),!.
-eval(X+Y,Z) :- eval(X,A), eval(Y,B), add(A,B,Z),!.
+eval(s(p(X)),Y) :- eval(X,Y),!.
+eval(p(s(X)),Y) :- eval(X,Y),!.
+eval(s(X+Y),s(Z)) :- eval(X,A), eval(Y,B), add(A,B,C), eval(C,Z),!.
+eval(p(X+Y),p(Z)) :- eval(X,A), eval(Y,B), add(A,B,C), eval(C,Z),!.
+eval(X+Y,Z) :- eval(X,A), eval(Y,B), add(A,B,C), eval(C,Z),!.
 eval(X,X):- numeral(X).
 
-add2(X,Y,Z) :- eval(X,A),eval(Y,B),add(A,B,Z).
+add2(X,Y,Z) :- eval(X,A),eval(Y,B),add(A,B,C), eval(C,Z).
