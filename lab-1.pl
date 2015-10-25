3f4571d2a9a68c62bef16931f9380dbb07241040
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2015-10-25 15:28:56 +0000

    Make eval more general

--- lab-1.pl
+++ lab-1.pl
@@ -3,6 +3,7 @@ numeral(s(X)) :- numeral(X).
 numeral(p(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 numeral(-X) :- numeral(X).
+numeral(X-Y) :- numeral(X), numeral(Y).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
@@ -10,10 +11,10 @@ add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
 eval(s(p(X)),Y) :- eval(X,Y),!.
 eval(p(s(X)),Y) :- eval(X,Y),!.
-eval(s(X+Y),s(Z)) :- eval(X,A), eval(Y,B), add(A,B,C), eval(C,Z),!.
-eval(p(X+Y),p(Z)) :- eval(X,A), eval(Y,B), add(A,B,C), eval(C,Z),!.
+eval(s(X),s(Y)) :- eval(X,Y),!.
+eval(p(X),p(Y)) :- eval(X,Y),!.
 eval(X+Y,Z) :- eval(X,A), eval(Y,B), add(A,B,C), eval(C,Z),!.
-eval(-X,Y) :- eval(X,A),neg(A,Y),!.
+eval(-(X),Y) :- eval(X,A),minus(A,Y),!.
 eval(X,X):- numeral(X).
 
 add2(X,Y,Z) :- eval(X,A),eval(Y,B),add(A,B,C), eval(C,Z).
