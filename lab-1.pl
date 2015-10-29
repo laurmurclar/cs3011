1f98c197fde8e7be334b87ed90ea0586b6a08276
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2015-10-29 20:45:42 +0000

    Remove unnecessary lines

--- lab-1.pl
+++ lab-1.pl
@@ -21,8 +21,6 @@ eval(X,X):- numeral(X).
 add2(X,Y,Z) :- eval(X,A),eval(Y,B),add(A,B,C), eval(C,Z).
 
 neg(0,0) :- !.
-neg(s(0), p(0)) :- !.
-neg(p(0), s(0)) :- !.
 neg(s(X),p(Y)) :- eval(X,A), neg(A,Y),!.
 neg(p(X),s(Y)) :- eval(X,A), neg(A,Y),!.
 
